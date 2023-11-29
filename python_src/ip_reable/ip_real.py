71b334d1c0a21f8d16e934ec0309b4cfcad06402
Erlone_alpha <125753378+Erlonealpha@users.noreply.github.com>
2023-11-29 11:11:07 +0800

    half done

--- python_src/ip_reable/ip_real.py
+++ python_src/ip_reable/ip_real.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from ping3 import ping, verbose_ping
 from concurrent.futures import ThreadPoolExecutor
+import os
 
 def respone_(ip):
     respones = requests.get(f'https://{ip}')
@@ -10,24 +11,33 @@ def respone_(ip):
         soup.title.string.strip() if soup.title else 'not title'
 
 def ping_ip(ip):
-    sec = ping(ip)
+    sec = ping(ip, timeout=0.05)
     if sec:
         ip_list.append(ip)
-        print(f'{ip} is reabled')
+        print(f'{ip} is reabled, time: {sec}')
     else:
-        print(f"{ip} is not reabled")
+        ip_ =int(ip.split('.')[3])
+        if ip_ % 20 == 0:
+            print(f"{ip} is not reabled")
+
 
 def main_ping():
-    with ThreadPoolExecutor(max_workers=8) as executor:
+    with ThreadPoolExecutor() as executor:
         futures = [executor.submit(ping_ip, ip) for ip in ip_list]
     for future in futures:
         future.result()
 
 
-if __name__ == '__man__':
+if __name__ == '__main__':
     tr = 18
     ip_list = []
     re_ip_list = []
     content_dict = {}
     ip_list =[f'172.{tr}.{x}.{y}' for x in range(1,256) for y in range(1,256)]
-    print(ip_list[:256])
+
+    main_ping()
+    if os.path.exists('./work'):
+        os.mkdir('./work')
+    with open('./work') as f:
+        for ip in ip_list:
+            f.write(f'{ip}\n')
